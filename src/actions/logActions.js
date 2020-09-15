73b378b6acd62eb10319d120486473ad5aa1f7d5
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-09-15 14:17:45 -0400

    search bar now searches logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS } from './types'
 
 // get logs from server
 export const getLogs = () => async dispatch=> {
@@ -39,6 +39,80 @@ export const addLog = (log) => async dispatch=> {
             type: LOGS_ERROR,
             payload: error.response.data
         })
+    }
+}
+
+export const deleteLog = (id) => async dispatch=> {
+    try {
+        setLoading();
+        await fetch(`/logs/${id}`, {
+            method: 'DELETE',
+        });
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        })
+    }
+};
+
+export const updateLog = (log) => async dispatch=> {
+    try {
+        setLoading();
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json();
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        })
+    }
+};
+
+export const searchLogs = (text) => async dispatch=> {
+    try {
+        setLoading();
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+        dispatch({
+            type: SEARCH_LOGS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        })
+    }
+}
+
+
+// set current log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT,
     }
 }
 
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions/types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS } from '../actions/types'
 
 const initialState = {
     logs: null,
@@ -14,6 +14,15 @@ export default(state = initialState, action) => {
                 ...state,
                 loading: true
             }
+        case SET_CURRENT: 
+            return {
+                ...state,
+                current: action.payload
+            }
+        case CLEAR_CURRENT:
+            return {
+                current: null
+            }
         case GET_LOGS: 
             return {
                 ...state,
@@ -25,6 +34,22 @@ export default(state = initialState, action) => {
                 ...state,
                 logs: [...state.logs, action.payload],
                 loading: false
+            }
+        case DELETE_LOG:
+            return {
+                ...state,
+                logs: state.logs.filter(log => log.id !== action.payload),
+                loading: false
+            }
+        case UPDATE_LOG:
+            return {
+                ...state,
+                logs: state.logs.map(log => log.id === action.payload.id ? action.payload : log)
+            }
+        case SEARCH_LOGS:
+            return {
+                ...state,
+                logs: action.payload
             }
         case LOGS_ERROR:
             console.error(action.payload);
